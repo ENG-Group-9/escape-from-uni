ea489b56a5c37a406f36ee66252afb3a84038b55
Sam Goff <sbm552@york.ac.uk>
2025-10-23 11:44:06 +0100

    Increased window size and changed viewport to remove black bars when fullscreen

--- core/src/main/java/group9/eng/Main.java
+++ core/src/main/java/group9/eng/Main.java
@@ -11,8 +11,8 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.Align; 
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -49,7 +49,7 @@ public class Main extends ApplicationAdapter {
     public void create() {
         physicsWorld = new World(new Vector2(0, 0), true);
         hitboxDebugRenderer = new Box2DDebugRenderer();
-        viewport = new FitViewport(200, 200);
+        viewport = new ExtendViewport(200, 200);
         camera = new Camera(viewport);
         entityManager = new EntityManager();
         map = new Map(physicsWorld, viewport);
