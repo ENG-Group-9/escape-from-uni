d40eadc6c121d9ac13902147714471998515f821
Titas Vaidila <titasvaidila2006@gmail.com>
2025-10-23 12:58:27 +0100

    Javadoc added for Camera class

--- core/src/main/java/group9/eng/Camera.java
+++ core/src/main/java/group9/eng/Camera.java
@@ -6,28 +6,48 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import group9.eng.components.BodyComponent;
 
+/**
+ * Represents a camera system that tracks a target entity or stationary positon.
+ * Works by having the camera smoothly transition to the target.
+ */
 public class Camera {
     private Viewport viewport;
-    
+
     private Vector3 targetPosVector3;
     private Vector2 targetPos;
     private BodyComponent targetBodyComponent;
 
+    /**
+     * Creates a Camera with a specified viewport.
+     * @param viewport the {@link Viewport} object used to control how the camera is managed.
+     */
     public Camera(Viewport viewport) {
         this.viewport = viewport;
         targetPos = new Vector2();
         targetPosVector3 = new Vector3();
     }
 
+    /**
+     * Sets an entity for the camera to follow.
+     * @param target the {@link Entity} the camera follows.
+     */
     public void setTarget(Entity target) {
         targetBodyComponent = (BodyComponent)target.getComponent(BodyComponent.class);
     }
 
+    /**
+     * Sets a fixed positon for the camera to focus on.
+     * @param targetPos the coordinates in the world for the camera to focus on.
+     */
     public void setTargetPos(Vector2 targetPos) {
         this.targetPos.set(targetPos);
         targetBodyComponent = null;
     }
 
+    /**
+     * Updates the cameras positon. If a target entity is set the camera moves gradually towards its position.
+     * Otherwise, the camera moves towards the fixed positon set by {@link #setTargetPos(Vector2)}
+     */
     public void update() {
         if (targetBodyComponent != null) {
             targetPos.set(targetBodyComponent.getPosition());
